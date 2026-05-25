12c6ec9f8747521e0de992f932fe2f5bfa5d13b1
Archana-WK <archanafutane03@gmail.com>
2026-05-25 15:45:56 +0530

    Fix smoothie fruit data display in Streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,4 +44,6 @@ if ingredients_list:
 # New section to displaysmoothiefroot nutition information
 import requests  
 smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(smoothiefroot_response).json()
+#st.text(smoothiefroot_response).json()
+sf_df = st.dataframe(data=smoothiefroot_response.jason(), use_container_width=True)
+
